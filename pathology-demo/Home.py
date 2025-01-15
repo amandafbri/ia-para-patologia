2e220b0d978c76996482f94979bec957f796c17d
amandafbri <amanda.brinhosa@gmail.com>
2025-01-15 17:44:50 -0300

    Add Streamlit application

---
+++ pathology-demo/Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import utils_streamlit
+
+st.set_page_config(page_title="Aplicações do Health AI Developer Foundations")
+
+st.title("Aplicações do Health AI Developer Foundations :stethoscope:")
+
+st.subheader("Demonstrações")
+st.write("Selecione uma página no menu do lado esquerdo para iniciar uma das demonstrações.")
+st.write(
+    """**1. Análise de imagens histopatológicas**: Experimente um modelo fine-tuned do Path Foundation.
+"""
+)
